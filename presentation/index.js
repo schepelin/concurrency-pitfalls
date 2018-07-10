c457fe35e51dec3faefb65f1a13259a22f0c1a34
Maxim Schepelin <m.schepelin@gmail.com>
2018-07-10 20:50:37 +0300

    Start with few unordered slides

--- presentation/index.js
+++ presentation/index.js
@@ -1,3 +1,5 @@
+import { css } from 'emotion'
+
 // Import React
 import React from "react"
 
@@ -5,14 +7,16 @@ import React from "react"
 import {
   BlockQuote,
   Cite,
+  Code,
   Deck,
-  Heading,
-  ListItem,
-  List,
-  Link,
-  Layout,
-  Notes,
   Fill,
+  Heading,
+  Image,
+  Layout,
+  Link,
+  List,
+  ListItem,
+  Notes,
   Quote,
   Slide,
   Text,
@@ -22,6 +26,7 @@ import CodeSlide from "spectacle-code-slide"
 
 // Import theme
 import createTheme from "spectacle/lib/themes/default"
+import preloader from "spectacle/lib/utils/preloader"
 
 // Require CSS
 require("normalize.css")
@@ -37,6 +42,19 @@ const theme = createTheme(colors, {
   primary: "Montserrat",
   secondary: "Helvetica",
 })
+const bodyStyle = css`
+  background: ${colors.secondary};
+`
+document.querySelector("body").classList.add(bodyStyle)
+
+const images = {
+}
+preloader(images)
+
+const code = {
+  dataRace: require("raw-loader!../assets/code/data_race.go"),
+}
+preloader(code)
 
 export default class Presentation extends React.Component {
   render() {
@@ -51,19 +69,73 @@ export default class Presentation extends React.Component {
         {/* Title slide */}
         <Slide transition={["fade"]} bgColor="primary">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Presentation Title
+            Concurrency Pitfalls
           </Heading>
           <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            Maxim Schepelin, Where, When
+            Maxim Schepelin, Gett RnD, 2018
           </Text>
         </Slide>
 
-        {/* Main slide. Copy it to have more */}
         <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
           <Notes>
             <p>Put the notes here</p>
           </Notes>
-          <Heading size={3} textColor="primary">Main slide</Heading>
+          <Heading size={3} textColor="tertiary">Race condition</Heading>
+          <Text textColor="primary">
+            Operations execution order
+          </Text>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <Heading size={3} textColor="tertiary">Definition</Heading>
+          <BlockQuote>
+            <Quote>
+              Two or more operations must execute in the correct order.<br/>
+              But, the program doesn't guarantee that order
+            </Quote>
+          </BlockQuote>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <Notes>
+            <p>Put the notes here</p>
+          </Notes>
+          <Heading size={3} textColor="tertiary">Data race</Heading>
+          <Text textColor="primary">Concurrent access to a memory address</Text>
+        </Slide>
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <Heading size={3} textColor="tertiary">Definition</Heading>
+          <BlockQuote>
+            <Quote>
+              More than one thread access the memory address and at least one of them is writing
+            </Quote>
+          </BlockQuote>
+        </Slide>
+
+        <CodeSlide
+          bgColor="secondary"
+          color={colors.primary}
+          transition={["fade"]}
+          lang="clike"
+          code={code.dataRace}
+          notes={`
+
+          `}
+          ranges={[
+            { loc: [5, 12], note: "Non determenistic behavior" },
+            { loc: [5, 6], note: "Declare variable" },
+            { loc: [6, 9], note: "Modify in the goroutine" },
+            { loc: [9, 12], note: "Read variable and print the value" },
+            { loc: [7, 8], note: "Critical section. Needs exclusive access to the variable" },
+            { loc: [9, 11], note: "Critical section. Requires exclusive read access" },
+          ]}
+        />
+
+        <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
+          <Heading size={3} textColor="tertiary">The difference</Heading>
+          <Text textColor="primary" fit bold>Data race – memory consistentcy</Text>
+          <Text textColor="primary" fit bold>Race condition – logical correctness</Text>
         </Slide>
 
         {/* last slide of a presentation */}
@@ -78,6 +150,7 @@ export default class Presentation extends React.Component {
             m.schepelin@gmail.com
           </Text>
         </Slide>
+
       </Deck>
     )
   }
